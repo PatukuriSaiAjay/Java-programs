1247eb985b71db9348c4ba3d2f7fc277a7084799
Patukuri Sai Ajay <patukurisaiajay21c@gmail.com>
2024-12-24 17:53:27 +0530

    pattern programming

---
+++ Pattern4.java
@@ -0,0 +1,19 @@
+public class Pattern4 
+{
+ public static void main(String[] args) 
+ {
+   int i;
+   int j;
+    
+   for(i=1;i<=5;i++)
+   {
+    for(j=1;j<=5-i+1;j++)
+    {
+        System.out.print("*");
+    }
+    System.out.println();
+   }
+   
+ }
+}   
+
