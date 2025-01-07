f2cb39dd78b62dceb20202da619f6a9eee86b2c1
Patukuri Sai Ajay <patukurisaiajay21c@gmail.com>
2025-01-07 19:53:51 +0530

    Single dimensional array

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -1,19 +1,46 @@
 import java.util.Scanner;
-class Array1
+
+public class Array1
 {
-public static void main(String args[])
-{
-Scanner scan=new Scanner(System.in);
-int[] a=new int[5];
-for(int i=0;i< a.length;i++)
-{
-System.out.println("enter the age");
-a[i]=scan.nextInt();
+	public static void main(String[] args) 
+	{
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the no.of students:");
+		int n=scan.nextInt();
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++)
+		{
+		    System.out.println("enter the ages of " +i +"'th student:");
+		    arr[i]=scan.nextInt();
+		}
+		
+		System.out.println("The ages of the student:");
+		
+		for(int i=0;i<n;i++)
+		{
+		    System.out.print(arr[i] + " ");
+		}
+	}
 }
-System.out.println("THE AGES ARE:");
-for(int i=0;i<a.length;i++)
-{
-System.out.println(a[i]);
-}
-}
-}
+
+//example of an output of an array
+
+/*
+
+Enter the no.of students:
+5
+enter the ages of 0'th student:
+20
+enter the ages of 1'th student:
+21
+enter the ages of 2'th student:
+22
+enter the ages of 3'th student:
+23
+enter the ages of 4'th student:
+22
+The ages of the student:
+20 21 22 23 22
+
+*/
