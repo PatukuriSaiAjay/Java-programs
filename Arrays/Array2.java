fc57a5bec36f5d70dfc21317b15b24b49225ce28
Patukuri Sai Ajay <patukurisaiajay21c@gmail.com>
2025-01-14 10:44:27 +0530

    Two dimensional array

--- Arrays/Array2.java
+++ Arrays/Array2.java
@@ -1,25 +1,77 @@
+//two dimensional array
+
 import java.util.Scanner;
+
 class Array2
 {
-public static void main(String args[])
-{
-Scanner scan=new Scanner(System.in);
-int[][] a=new int[2][5];
-for (int i=0;i<a.length;i++)
-{
-for(int j=0;j<a[i].length;j++)
-{
-System.out.println("enter the student age in classes " + i + " students " +j);
-a[i][j]=scan.nextInt();
+    public static void main(String args[])
+    {
+        int m,n;
+        
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Enter the no.of classes:");
+        m=scan.nextInt();
+        System.out.println("Enter the no.of students:");
+        n=scan.nextInt();
+        
+        int [][] a= new int[m][n];
+        
+        //storing
+        
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a[i].length;j++)
+            {
+                System.out.println("Enter the age of " + (i+                             1) + " class " +(j+1) + " student: ");
+                a[i][j]=scan.nextInt();
+            }
+        }
+        
+        System.out.println("The ages of the student:");
+        //printing
+        
+           for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a[i].length;j++)
+            {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
 }
-}
-System.out.println("the student ages in classroom:");
-for (int i=0;i<a.length;i++)
-{
-for(int j=0;j<a[i].length;j++)
-{
-System.out.println(a[i][j]);
-}
-}
-}
-}
+
+
+//output
+
+/*
+Enter the no.of classes:
+2
+Enter the no.of students:
+5
+Enter the age of 1 class 1 student:
+20
+Enter the age of 1 class 2 student:
+21
+Enter the age of 1 class 3 student:
+22
+Enter the age of 1 class 4 student:
+23
+Enter the age of 1 class 5 student:
+24
+Enter the age of 2 class 1 student:
+24
+Enter the age of 2 class 2 student:
+23
+Enter the age of 2 class 3 student:
+22
+Enter the age of 2 class 4 student:
+21
+Enter the age of 2 class 5 student:
+20
+The ages of the student:
+20 21 22 23 24
+24 23 22 21 20
+
+*/
